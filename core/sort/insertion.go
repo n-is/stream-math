539791cd2734fadcb28c27c7340d74e5f3b5dfb0
n-is <nschl.npl@gmail.com>
2020-08-02 14:26:53 +0545

    Add insertion sort algorithm implementation for stream.

---
+++ core/sort/insertion.go
@@ -0,0 +1,57 @@
+package sort
+
+import "container/list"
+
+type Insertion struct {
+	less    func(old, new interface{}) bool
+	ordered *list.List
+}
+
+func NewInsertion(less func(old, new interface{}) bool) *Insertion {
+	return &Insertion{
+		less:    less,
+		ordered: list.New(),
+	}
+}
+
+func (ins *Insertion) Add(v interface{}) {
+	// Perform insertion
+	if ins.ordered.Len() == 0 {
+		ins.ordered.PushBack(v)
+		return
+	}
+
+	for e := ins.First(); e != nil; e = e.Next() {
+		old := e.Value
+		if ins.less(old, v) {
+			ins.ordered.InsertBefore(v, e)
+			return
+		}
+	}
+	ins.ordered.PushBack(v)
+}
+
+func (ins *Insertion) First() *list.Element {
+	return ins.ordered.Front()
+}
+
+func (ins *Insertion) Last() *list.Element {
+	return ins.ordered.Back()
+}
+
+func (ins *Insertion) Remove(v interface{}) {
+	for e := ins.First(); e != nil; e = e.Next() {
+		if e.Value == v {
+			ins.ordered.Remove(e)
+			break
+		}
+	}
+}
+
+func (ins *Insertion) Reset() {
+	for e := ins.First(); e != nil; {
+		v := e.Next()
+		ins.ordered.Remove(e)
+		e = v
+	}
+}
