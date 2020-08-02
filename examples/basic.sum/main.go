87575c606dd673b56b5f66bb04f16ae38bffefe0
n-is <nschl.npl@gmail.com>
2020-08-02 14:28:23 +0545

    Update examples.

--- examples/basic.sum/main.go
+++ examples/basic.sum/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/n-is/stream-math/core/basic"
+	"github.com/n-is/stream-math/core/numeric"
 )
 
 func main() {
-	sum := basic.NewSum()
+	sum := numeric.NewSum()
 
 	data := []int{
 		1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
